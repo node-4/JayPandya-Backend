148c2037430a550707a66efc47e97cc5eceb5dc2
rubiyadav <rubiflyeis@gmail.com>
2023-06-16 12:22:13 +0530

    update

--- server.js
+++ server.js
@@ -1,77 +1,35 @@
-const express = require('express');
-// const helmet = require('helmet');
-// const xss = require('xss-clean');
-const compression = require('compression');
-const cors = require('cors');
-// const mongoSanitize = require('express-mongo-sanitize');
-const swaggerUI = require('swagger-ui-express');
-const env = require('dotenv');
-const createLocaleMiddleware = require('express-locale');
-// const startPolyglot = require('./src/utils/startPolyglot');
-const docs = require('./docs/swagger');
-const routes = require('./src/routes');
-const serverless = require('serverless-http')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
-app.enable('trust proxy');
-app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
-app.use(
-  createLocaleMiddleware({
-    priority: ['accept-language', 'default'],
-    default: 'en_US'
-  })
-);
-// app.use(startPolyglot);
-// app.use(xss());
-// app.use(mongoSanitize());
+const http = require('http');
+const server = http.createServer(app);
+const bodyparser = require("body-parser");
+const serverless = require("serverless-http");
+const docs = require('./docs/swagger');
+const swaggerUI = require('swagger-ui-express');
+const routes = require('./src/routes');
+require("dotenv").config();
 app.use(cors());
-app.options('*', cors());
-app.use(compression());
-app.disable('x-powered-by');
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  if (err.status) {
-      console.log(err);
-      console.log('error middleware');
-      return res.status(err.status).json({
-          msg: err.message
-      })
-
-  } else {
-
-      console.log(err);
-      console.log('error middleware status not given');
-      return res.status(500).json({
-          msg: err.message
-      })
-  }
-
-})
-
-
-
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: "App Start"
-  })
-})
-app.use('/api', routes);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(docs));
-const mongoose = require('mongoose');
+app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 2006;
 mongoose.connect("mongodb+srv://jaypanda:jaypanda@jay.uvoxkja.mongodb.net/?retryWrites=true&w=majority").then(() => {
   console.log("Db conneted succesfully");
 }).catch((err) => {
   console.log(err);
 });
-
-const serverPort = process.env.PORT;
-app.listen(serverPort, () => {
-  console.log(`listening on port ${serverPort}`);
+app.use('/api', routes);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(docs));
+app.get("/", (req, res) => {
+  res.status(200).send({ msg: "Working App" });
 });
+
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
 module.exports = {
-  handler: serverless(app)
-}
+  handler: serverless(app),
+};
